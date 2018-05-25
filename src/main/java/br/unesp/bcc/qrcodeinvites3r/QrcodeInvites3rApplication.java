5f0d89c171c42ec4c04f69a327102db1a2658a30
Alex Garcia <agarcia46@dxc.com>
2018-05-25 18:36:53 -0300

    Estrutura inicial gerada pelo Spring Initializr.

---
+++ src/main/java/br/unesp/bcc/qrcodeinvites3r/QrcodeInvites3rApplication.java
@@ -0,0 +1,12 @@
+package br.unesp.bcc.qrcodeinvites3r;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeInvites3rApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeInvites3rApplication.class, args);
+	}
+}
